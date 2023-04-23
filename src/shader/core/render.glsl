0f2dbdd882849905b5a9188d1968dbf65f3092d6
Nugroho Dewantorto <nugroho.dewantoro@gmail.com>
2023-04-23 12:23:55 +0700

    little tidying up

--- src/shader/core/render.glsl
+++ src/shader/core/render.glsl
@@ -12,3 +12,10 @@ vec3 integrandOverArea(vec3 color, float brdf, float NoL, float NloL, float squa
 vec3 partialIntegrand(vec3 color, float brdf, float NoL) {
   return color * brdf * NoL;
 }
+
+float radiance(Ray r, HitRecord hit, uint lightIndex) {
+  float sqrDistance = hit.t * hit.t * dot(r.direction, r.direction);
+  float cosine = max(dot(hit.faceNormal.normal, -1.0 * normalize(r.direction)), 0.001);
+
+  return cosine * areaTriangle(lights[lightIndex].triangle) / sqrDistance;
+}
