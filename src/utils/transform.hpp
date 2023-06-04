ca702acc919bd36de0ee1b4ea0c2fc4fa42bb392
Nugroho Dewantorto <nugroho.dewantoro@gmail.com>
2023-06-04 09:06:35 +0700

    fix

--- src/utils/transform.hpp
+++ src/utils/transform.hpp
@@ -5,12 +5,12 @@
 
 namespace nugiEngine {
 	struct TransformComponent {
-		glm::vec3 translation{};
-		glm::vec3 scale{1.0f, 1.0f, 1.0f};
-		glm::vec3 rotation{};
+		glm::vec4 translation{0.0f};
+		glm::vec4 scale{1.0f};
+		glm::vec4 rotation{0.0f};
 
-		glm::mat3 rotationMatrix();
-    glm::mat3 inverserotationMatrix();
-		glm::mat3 normalMatrix();
+		glm::mat4 rotationMatrix();
+    glm::mat4 inverserotationMatrix();
+		glm::mat4 normalMatrix();
 	};
 }
