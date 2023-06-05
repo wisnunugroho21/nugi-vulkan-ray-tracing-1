576b5a0ca69bc06f1b42b620bd0b6180fd5c14b4
Nugroho Dewantorto <nugroho.dewantoro@gmail.com>
2023-06-06 03:23:21 +0700

    update rotate transform

--- src/utils/transform.cpp
+++ src/utils/transform.cpp
@@ -4,25 +4,25 @@ namespace nugiEngine {
   glm::mat3 TransformComponent::rotationMatrix() {
     const float c3 = glm::cos(this->rotation.z);
     const float s3 = glm::sin(this->rotation.z);
-    const float c2 = glm::cos(this->rotation.x);
-    const float s2 = glm::sin(this->rotation.x);
-    const float c1 = glm::cos(this->rotation.y);
-    const float s1 = glm::sin(this->rotation.y);
+    const float c2 = glm::cos(this->rotation.y);
+    const float s2 = glm::sin(this->rotation.y);
+    const float c1 = glm::cos(this->rotation.x);
+    const float s1 = glm::sin(this->rotation.x);
 
     return glm::mat3{
       {
-        (c1 * c3 + s1 * s2 * s3),
-        (c2 * s3),
-        (c1 * s2 * s3 - c3 * s1)
+        (s1 * s2 * c3 - c1 * s3),
+        (s1 * s2 * s3 + c1 * c3),
+        (s1 * c2)
       },
       {
-        (c3 * s1 * s2 - c1 * s3),
         (c2 * c3),
-        (c1 * c3 * s2 + s1 * s3)
+        (c2 * s3),
+        (-s2)
       },
       {
-        (c2 * s1),
-        (-s2),
+        (c1 * s2 * c3 + s1 * s3),
+        (c1 * s2 * s3 - s1 * c3),
         (c1 * c2)
       }
     };
