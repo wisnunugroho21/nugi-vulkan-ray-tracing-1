48dd5f6df6f12d3c263939e91f03dba46808c85e
Nugroho Dewantorto <nugroho.dewantoro@gmail.com>
2023-07-02 18:03:16 +0700

    fix bug

--- compile.sh
+++ compile.sh
@@ -1,2 +1,5 @@
 glslc src/shader/ray_trace.comp -o bin/shader/ray_trace.comp.spv
+glslc src/shader/sampling.vert -o bin/shader/sampling.vert.spv
 glslc src/shader/sampling.frag -o bin/shader/sampling.frag.spv
+glslc src/shader/forward_pass.vert -o bin/shader/forward_pass.vert.spv
+glslc src/shader/forward_pass.frag -o bin/shader/forward_pass.frag.spv
