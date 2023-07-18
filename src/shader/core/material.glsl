4e2f66275627a59a6d604082599d6a74f911c97c
Nugroho Dewantorto <nugroho.dewantoro@gmail.com>
2023-07-19 03:31:31 +0700

    fix bug

--- src/shader/core/material.glsl
+++ src/shader/core/material.glsl
@@ -109,14 +109,32 @@ vec3 randomCosineDirection(uint additionalRandomSeed) {
   float r1 = randomFloat(additionalRandomSeed);
   float r2 = randomFloat(additionalRandomSeed + 1);
 
-  float phi = 2 * 3.14159265359 * r1;
+  /* float phi = 2 * 3.14159265359 * r1;
   float cosTheta = sqrt(r2);
   
   float x = cos(phi) * cosTheta;
   float y = sin(phi) * cosTheta;
-  float z = sqrt(1 - r2);
+  float z = sqrt(1 - r2); */
 
-  return vec3(x, y, z);
+  vec2 offset = 2.0f * vec2(r1, r2) - vec2(1.0f);
+  if (offset.x == 0 && offset.y == 0) return vec3(0.0f, 0.0f, 1.0f);
+
+  float theta, r;
+  float PiOver4 = 0.78539816339744830961;
+  float PiOver2 = 1.57079632679489661923;
+
+  if (abs(offset.x) > abs(offset.y)) {
+    r = offset.x;
+    theta = PiOver4 * (offset.y / offset.x);
+  } else {
+    r = offset.y;
+    theta = PiOver2 - PiOver4 * (offset.x / offset.y);
+  }
+
+  offset = r * vec2(cos(theta), sin(theta));
+  float z = sqrt(1.0f - pow(offset.x, 2) - pow(offset.y, 2));
+
+  return vec3(offset.x, offset.y, z);
 }
 
 vec3 lambertRandomDirection(vec3 normal, uint additionalRandomSeed) {
