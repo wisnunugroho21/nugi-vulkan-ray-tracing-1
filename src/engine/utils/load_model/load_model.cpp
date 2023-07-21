2acdfbc7e19f797c87714cf419c69ee8e33d4352
Nugroho Dewantorto <nugroho.dewantoro@gmail.com>
2023-07-21 15:12:25 +0700

    use multithread to load model

--- src/engine/utils/load_model/load_model.cpp
+++ src/engine/utils/load_model/load_model.cpp
@@ -5,103 +5,117 @@
 
 namespace nugiEngine
 {
+	void loadModel(std::shared_ptr<tinyobj::attrib_t> attrib, std::shared_ptr<std::vector<tinyobj::shape_t>> shapes, uint32_t shapeIndex, uint32_t triangleIndex, 
+		std::shared_ptr<std::vector<Primitive>> outPrimitives, std::shared_ptr<std::vector<RayTraceVertex>> outVertices, uint32_t materialIndex, uint32_t offsetIndex) 
+	{
+		int vertexIndex0 = (*shapes)[shapeIndex].mesh.indices[3 * triangleIndex + 0].vertex_index;
+		int vertexIndex1 = (*shapes)[shapeIndex].mesh.indices[3 * triangleIndex + 1].vertex_index;
+		int vertexIndex2 = (*shapes)[shapeIndex].mesh.indices[3 * triangleIndex + 2].vertex_index;
+
+		int textCoordIndex0 = (*shapes)[shapeIndex].mesh.indices[3 * triangleIndex + 0].texcoord_index;
+		int textCoordIndex1 = (*shapes)[shapeIndex].mesh.indices[3 * triangleIndex + 1].texcoord_index;
+		int textCoordIndex2 = (*shapes)[shapeIndex].mesh.indices[3 * triangleIndex + 2].texcoord_index;
+
+		RayTraceVertex vertex0;
+		RayTraceVertex vertex1;
+		RayTraceVertex vertex2;
+		
+		vertex0.position = glm::vec3{
+			attrib->vertices[3 * vertexIndex0 + 0],
+			attrib->vertices[3 * vertexIndex0 + 1],
+			attrib->vertices[3 * vertexIndex0 + 2]
+		};
+
+		vertex1.position = glm::vec3{
+			attrib->vertices[3 * vertexIndex1 + 0],
+			attrib->vertices[3 * vertexIndex1 + 1],
+			attrib->vertices[3 * vertexIndex1 + 2]
+		};
+
+		vertex2.position = glm::vec3{
+			attrib->vertices[3 * vertexIndex2 + 0],
+			attrib->vertices[3 * vertexIndex2 + 1],
+			attrib->vertices[3 * vertexIndex2 + 2]
+		};
+
+		vertex0.texel = glm::vec2{
+			attrib->texcoords[2 * textCoordIndex0 + 0],
+			1.0f - attrib->texcoords[2 * textCoordIndex0 + 1]
+		};
+
+		vertex1.texel = glm::vec2{
+			attrib->texcoords[2 * textCoordIndex1 + 0],
+			1.0f - attrib->texcoords[2 * textCoordIndex1 + 1]
+		};
+
+		vertex2.texel = glm::vec2{
+			attrib->texcoords[2 * textCoordIndex2 + 0],
+			1.0f - attrib->texcoords[2 * textCoordIndex2 + 1]
+		};
+
+		/* if (uniqueVertices.count(vertex0) == 0) {
+			uniqueVertices[vertex0] = static_cast<uint32_t>(vertices->size()) + offsetIndex;
+			vertices->emplace_back(vertex0);
+		}
+
+		if (uniqueVertices.count(vertex1) == 0) {
+			uniqueVertices[vertex1] = static_cast<uint32_t>(vertices->size()) + offsetIndex;
+			vertices->emplace_back(vertex1);
+		}
+
+		if (uniqueVertices.count(vertex2) == 0) {
+			uniqueVertices[vertex2] = static_cast<uint32_t>(vertices->size()) + offsetIndex;
+			vertices->emplace_back(vertex2);
+		}
+
+		uint32_t index0 = uniqueVertices[vertex0];
+		uint32_t index1 = uniqueVertices[vertex1];
+		uint32_t index2 = uniqueVertices[vertex2]; */
+
+		uint32_t index0 = static_cast<uint32_t>(outVertices->size()) + offsetIndex;
+		outVertices->emplace_back(vertex0);
+
+		uint32_t index1 = static_cast<uint32_t>(outVertices->size()) + offsetIndex;
+		outVertices->emplace_back(vertex1);
+
+		uint32_t index2 = static_cast<uint32_t>(outVertices->size()) + offsetIndex;
+		outVertices->emplace_back(vertex2);
+
+		outPrimitives->emplace_back(Primitive{
+			glm::uvec3{ index0, index1, index2 },
+			materialIndex
+		});
+	}
+
   LoadedModel loadModelFromFile(const std::string &filePath, uint32_t materialIndex, uint32_t offsetIndex) {
-		tinyobj::attrib_t attrib{};
-		std::vector<tinyobj::shape_t> shapes{};
-		std::vector<tinyobj::material_t> materials{};
+		auto attrib = std::make_shared<tinyobj::attrib_t>();
+		auto shapes = std::make_shared<std::vector<tinyobj::shape_t>>();
+		auto materials = std::make_shared<std::vector<tinyobj::material_t>>();
 		// std::unordered_map<RayTraceVertex, uint32_t> uniqueVertices{};
 		std::string warn, err;
 
-		if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filePath.c_str())) {
+		if (!tinyobj::LoadObj(attrib.get(), shapes.get(), materials.get(), &warn, &err, filePath.c_str())) {
 			throw std::runtime_error(warn + err);
 		}
 		
 		auto primitives = std::make_shared<std::vector<Primitive>>();
 		auto vertices = std::make_shared<std::vector<RayTraceVertex>>();
 
-		for (const auto &shape: shapes) {
-			uint32_t numTriangle = static_cast<uint32_t>(shape.mesh.indices.size()) / 3;
+		std::vector<std::thread> threads;
 
-			for (uint32_t i = 0; i < numTriangle; i++) {
-				int vertexIndex0 = shape.mesh.indices[3 * i + 0].vertex_index;
-				int vertexIndex1 = shape.mesh.indices[3 * i + 1].vertex_index;
-				int vertexIndex2 = shape.mesh.indices[3 * i + 2].vertex_index;
+		for (uint32_t i = 0; i < (*shapes).size(); i++) {
+			uint32_t numTriangle = static_cast<uint32_t>((*shapes)[i].mesh.indices.size()) / 3;
 
-				int textCoordIndex0 = shape.mesh.indices[3 * i + 0].texcoord_index;
-				int textCoordIndex1 = shape.mesh.indices[3 * i + 1].texcoord_index;
-				int textCoordIndex2 = shape.mesh.indices[3 * i + 2].texcoord_index;
-
-				RayTraceVertex vertex0;
-				RayTraceVertex vertex1;
-				RayTraceVertex vertex2;
-				
-				vertex0.position = glm::vec3{
-					attrib.vertices[3 * vertexIndex0 + 0],
-					attrib.vertices[3 * vertexIndex0 + 1],
-					attrib.vertices[3 * vertexIndex0 + 2]
-				};
-
-				vertex1.position = glm::vec3{
-					attrib.vertices[3 * vertexIndex1 + 0],
-					attrib.vertices[3 * vertexIndex1 + 1],
-					attrib.vertices[3 * vertexIndex1 + 2]
-				};
-
-				vertex2.position = glm::vec3{
-					attrib.vertices[3 * vertexIndex2 + 0],
-					attrib.vertices[3 * vertexIndex2 + 1],
-					attrib.vertices[3 * vertexIndex2 + 2]
-				};
-
-				vertex0.texel = glm::vec2{
-					attrib.texcoords[2 * textCoordIndex0 + 0],
-					1.0f - attrib.texcoords[2 * textCoordIndex0 + 1]
-				};
-
-				vertex1.texel = glm::vec2{
-					attrib.texcoords[2 * textCoordIndex1 + 0],
-					1.0f - attrib.texcoords[2 * textCoordIndex1 + 1]
-				};
-
-				vertex2.texel = glm::vec2{
-					attrib.texcoords[2 * textCoordIndex2 + 0],
-					1.0f - attrib.texcoords[2 * textCoordIndex2 + 1]
-				};
-
-				/* if (uniqueVertices.count(vertex0) == 0) {
-					uniqueVertices[vertex0] = static_cast<uint32_t>(vertices->size()) + offsetIndex;
-					vertices->emplace_back(vertex0);
-				}
-
-				if (uniqueVertices.count(vertex1) == 0) {
-					uniqueVertices[vertex1] = static_cast<uint32_t>(vertices->size()) + offsetIndex;
-					vertices->emplace_back(vertex1);
-				}
-
-				if (uniqueVertices.count(vertex2) == 0) {
-					uniqueVertices[vertex2] = static_cast<uint32_t>(vertices->size()) + offsetIndex;
-					vertices->emplace_back(vertex2);
-				}
-
-				uint32_t index0 = uniqueVertices[vertex0];
-				uint32_t index1 = uniqueVertices[vertex1];
-				uint32_t index2 = uniqueVertices[vertex2]; */
-
-				uint32_t index0 = static_cast<uint32_t>(vertices->size()) + offsetIndex;
-				vertices->emplace_back(vertex0);
-
-				uint32_t index1 = static_cast<uint32_t>(vertices->size()) + offsetIndex;
-				vertices->emplace_back(vertex1);
-
-				uint32_t index2 = static_cast<uint32_t>(vertices->size()) + offsetIndex;
-				vertices->emplace_back(vertex2);
-
-				primitives->emplace_back(Primitive{
-					glm::uvec3{ index0, index1, index2 },
-					materialIndex
-				});
+			for (uint32_t j = 0; j < numTriangle; j++) {
+				std::thread th(loadModel, attrib, shapes, i, j, primitives, vertices, materialIndex, offsetIndex);
+				threads.emplace_back(std::move(th));
 			}
 		}
+
+		for (auto &&thread : threads) {
+			thread.join();
+		}
+		
 
 		return LoadedModel{ primitives, vertices };
 	}
--- src/engine/utils/load_model/load_model.hpp
+++ src/engine/utils/load_model/load_model.hpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <memory>
 #include <vector>
+#include <thread>
+#include <utility>
 
 namespace nugiEngine
 {
